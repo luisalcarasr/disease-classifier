cd1bac5c7132ee345389a7363f47b50f0c296d22
Luis Alcaras <luisalcarasr@gmail.com>
2021-05-06 23:13:59 -0500

    The API for eGFR has been implemented. (#4)

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import { getHypertensionStage } from '../utils/getHypertensionStage';
+import { getKidneyDiseaseClassification } from '../utils/getKidneyDiseaseClassification';
 import { db } from './database';
 
 const app = express();
@@ -13,21 +14,21 @@ app.get('/', (req, res) => {
 app.get('/blood-pressures', (req, res) => {
   db.all('SELECT * FROM BLOOD_PRESSURES ORDER BY atDate DESC', [], (error, bloodPressures) => {
     if (error) {
-      res.statusCode(500);
-      res.json(error);
+      res.status(500).json(error);
+    } else {
+      res.json(bloodPressures);
     }
-    res.json(bloodPressures);
   });
 });
 
 app.get('/blood-pressures/last', (req, res) => {
   db.get('SELECT * FROM BLOOD_PRESSURES ORDER BY atDate DESC', [], (error, bloodPressure) => {
     if (error) {
-      res.statusCode(500);
-      res.json(error);
+      res.status(500).json(error);
+    } else {
+      bloodPressure.classification = getHypertensionStage(bloodPressure.SysBP, bloodPressure.DiaBP);
+      res.json(bloodPressure);
     }
-    bloodPressure.classification = getHypertensionStage(bloodPressure.SysBP, bloodPressure.DiaBP);
-    res.json(bloodPressure);
   });
 });
 
@@ -38,14 +39,47 @@ app.post('/blood-pressures', (req, res) => {
     [bp.SysBP, bp.DiaBP, bp.atDate],
     (results, error) => {
       if (!error) {
-        res.statusCode(201);
-        res.json(bp);
+        res.status(201).json(bp);
       } else {
-        res.statusCode(500);
-        res.json(error);
+        res.status(500).json(error);
       }
     }
   );
+});
+
+app.get('/e-gfr', (req, res) => {
+  db.all('SELECT * FROM E_GFR ORDER BY atDate DESC', [], (error, rates) => {
+    if (error) {
+      res.statusCode(500);
+      res.json(error);
+    } else {
+      res.json(rates);
+    }
+  });
+});
+
+app.get('/e-gfr/last', (req, res) => {
+  db.get('SELECT * FROM E_GFR ORDER BY atDate DESC', [], (error, rate) => {
+    if (error) {
+      res.statusCode(500);
+      res.json(error);
+    } else {
+      rate.classification = getKidneyDiseaseClassification(rate.eGFR);
+      res.json(rate);
+    }
+  });
+});
+
+app.post('/e-gfr', (req, res) => {
+  const rate = req.body;
+  db.run('INSERT INTO E_GFR (eGFR, atDate) VALUES(?, ?)', [rate.eGFR, rate.atDate], (result, error) => {
+    if (!error) {
+      res.status(201).json(rate);
+    } else {
+      res.statusCode(500);
+      res.json(error);
+    }
+  });
 });
 
 export { app };
--- src/api/migrations.js
+++ src/api/migrations.js
@@ -4,8 +4,13 @@ const createBloodPressuresTable = () => {
   db.run('CREATE TABLE BLOOD_PRESSURES ( SysBP REAL, DiaBP REAL, atDate TEXT)');
 };
 
+const createEGFRTable = () => {
+  db.run('CREATE TABLE E_GFR ( eGFR REAL, atDate TEXT)');
+};
+
 const migrate = () => {
   createBloodPressuresTable();
+  createEGFRTable();
 };
 
 migrate();
